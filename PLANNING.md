e91392b4810d3835b433b8d9a4ec04317c3dbdea
Carson Banov <carson@spanishdict.com>
2015-04-15 16:07:54 -0400

    Clicking actually expands/collapses.

--- PLANNING.md
+++ PLANNING.md
@@ -4,3 +4,5 @@
 * include images for on page
 * local storage, cookies, etc. for preserving state of comment (expanded/collapsed)
 * probably want to do button on comments on the code not just "issuecomment"
+* re-loop after someone submits a comment to add new button (need to check if button exists)
+*
--- gmrcc.js
+++ gmrcc.js
@@ -8,8 +8,44 @@
 
   // Todo: loop thru all div[id^=issuecomment-] and collapse any items that should be collapsed
 
-  $("div[id^=issuecomment-] .timeline-comment-label").each(function(){
-    $(this).after("<span class='gmrcc-toggle timeline-comment-label'>image here</span>");
+  $("div[id^=issuecomment-] .timeline-comment-actions").each(function(){
+
+    // Set up button for expanding/collapsing.
+    var $toggle = $("<span class='gmrcc-toggle timeline-comment-label'>image here</span>")
+
+    // Add button to comment header space.
+    $(this).after($toggle);
+
+
+    // Set up variable to represent state of expanded/collapsed (0 collapsed,
+    // 1 is expanded).
+    var doneState = 1;
+    // Keep track of parent comment and comment.
+    var $parent = $(this).parents("div[id^=issuecomment-]");
+    var $comment = $parent.find(".comment-content");
+    var $header = $parent.find(".timeline-comment-header");
+    var colorPrev = $header.css("background-color");
+    var borderPrev = $parent.css("border");
+
+    // When clicked, toggle should expand/collapse the comment.
+    //
+    $toggle.click(function() {
+      if(doneState==1){
+        // Collapse elements.
+        $parent.css("border", "none");
+        $header.css("background-color", "white");
+        $comment.hide();
+        doneState = 0;
+      }
+      else {
+        // Expand elements.
+        $parent.css("border", borderPrev);
+        $header.css("background-color", colorPrev);
+        $comment.show();
+        doneState = 1;
+      }
+    });
+
 
     // Todo: as we loop thru gmrcc-toggle elements to set up a click handler that does something
     // something should be:
